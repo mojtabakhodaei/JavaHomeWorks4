cab2895beb86edcff090c0e276972f56963a13b8
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 22:37:12 +0330

    Makes more readable

--- TestPatient.java
+++ TestPatient.java
@@ -2,10 +2,12 @@ package HomeWork4;
 
 public class TestPatient {
     public static void main(String[] args) {
+        System.out.println("Patient 1 :");
         Patient p1 = new Patient();
         System.out.println("ID: " + p1.getIdNumbrer() + "\nAge: " + p1.getAge());
-        System.out.println("BloodType: " + p1.getBloodData().getBloodtype() + ((p1.getBloodData().getRHFactor()) ? "+" : "-"));
+        System.out.println("BloodType: " + p1.getBloodData().getBloodtype() + ((p1.getBloodData().getRHFactor()) ? "+" : "-") + "\n=====================");
 
+        System.out.println("Patient 2 :");
         Patient p2 = new Patient(12 , 24 , new BloodData("A" , false));
         System.out.println("ID: " + p2.getIdNumbrer() + "\nAge: " + p2.getAge());
         System.out.println("BloodType: " + p2.getBloodData().getBloodtype() + ((p2.getBloodData().getRHFactor()) ? "+" : "-"));
