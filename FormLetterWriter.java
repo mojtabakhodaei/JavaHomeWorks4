d241a39d8cf6361ce18f2383f98b9f612c8424a6
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 17:11:12 +0330

    Add "Thank you for your recent order" in the of each salutation method printing

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -2,10 +2,10 @@ package HomeWork4;
 
 public class FormLetterWriter {
     void displaySalutation(String lastName) {
-        System.out.println("salutation Dear mr. or Ms. " + lastName);
+        System.out.println("salutation Dear mr. or Ms. " + lastName + "Thank you for your recent order");
     }
 
     void displaySalutation(String firstName, String lastName) {
-        System.out.println("greeting Dear " + firstName + ' ' + lastName);
+        System.out.println("greeting Dear " + firstName + ' ' + lastName + "Thank you for your recent order");
     }
 }
