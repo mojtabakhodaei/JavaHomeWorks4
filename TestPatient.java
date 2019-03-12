ff090e84de488dd746eb9de93f294b9952f346c9
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 22:28:12 +0330

    Fixes Testpatient class

--- TestPatient.java
+++ TestPatient.java
@@ -4,6 +4,6 @@ public class TestPatient {
     public static void main(String[] args) {
         Patient p1=new Patient();
         System.out.println("ID: " + p1.getIdNumbrer() + "\nAge: " + p1.getAge() );
-        System.out.println("BloodType: " + p1.getBloodData().Bloodtype +  ((p1.getBloodData().RHFactor)?  "+" : "-"));
+        System.out.println("BloodType: " + p1.getBloodData().getBloodtype() +  ((p1.getBloodData().getRHFactor())?  "+" : "-"));
     }
 }
