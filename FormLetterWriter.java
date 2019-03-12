babf9cdc96f5843bbec9f814d0488b88f646837d
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 17:13:07 +0330

    Change both method type to Static

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -1,11 +1,12 @@
 package HomeWork4;
 
 public class FormLetterWriter {
-    void displaySalutation(String lastName) {
+    static void displaySalutation(String lastName) {
         System.out.println("salutation Dear mr. or Ms. " + lastName + "Thank you for your recent order");
     }
 
-    void displaySalutation(String firstName, String lastName) {
+    static void displaySalutation(String firstName, String lastName) {
         System.out.println("greeting Dear " + firstName + ' ' + lastName + "Thank you for your recent order");
     }
+
 }
