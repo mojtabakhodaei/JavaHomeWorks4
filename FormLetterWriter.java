c672dae664585fc65294fc6bfb6e51c5ccdc0d1c
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 17:17:51 +0330

    Check program and fixed small bugs

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -2,11 +2,11 @@ package HomeWork4;
 
 public class FormLetterWriter {
     static void displaySalutation(String lastName) {
-        System.out.println("salutation Dear mr. or Ms. " + lastName + "Thank you for your recent order");
+        System.out.println("salutation Dear mr. or Ms. " + lastName + " Thank you for your recent order");
     }
 
     static void displaySalutation(String firstName, String lastName) {
-        System.out.println("greeting Dear " + firstName + ' ' + lastName + "Thank you for your recent order");
+        System.out.println("greeting Dear " + firstName + ' ' + lastName + " Thank you for your recent order");
     }
 
     public static void main(String[] args) {
