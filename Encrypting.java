c898a3893e422aacdac949c3ed31d888853377f8
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 15:47:44 +0330

    add method encryptAll for encrypt all character in string

--- Encrypting.java
+++ Encrypting.java
@@ -14,8 +14,16 @@ public class Encrypting {
         System.out.println("Write your number for encrypt: ");
         number = scanner.nextInt();
     }
+
     static char encrypt(char charactor, int codeNumber) {
-        return (char)((int) charactor+ codeNumber);
+        return (char) ((int) charactor + codeNumber);
     }
 
+    static String encryptAll(String str, int number) {
+        String str2;
+        for (int i = 0; i <= str.length(); i++) {
+            str2 = str.replace(str.charAt(i), encrypt(str.charAt(i), number));
+        }
+        return str2;
+    }
 }
