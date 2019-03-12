ea544bd28254e114b20655f870351d3fe4515897
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 22:32:42 +0330

    Declare p2 as new patient and print its praperties

--- TestPatient.java
+++ TestPatient.java
@@ -2,8 +2,13 @@ package HomeWork4;
 
 public class TestPatient {
     public static void main(String[] args) {
-        Patient p1=new Patient();
-        System.out.println("ID: " + p1.getIdNumbrer() + "\nAge: " + p1.getAge() );
-        System.out.println("BloodType: " + p1.getBloodData().getBloodtype() +  ((p1.getBloodData().getRHFactor())?  "+" : "-"));
+        Patient p1 = new Patient();
+        System.out.println("ID: " + p1.getIdNumbrer() + "\nAge: " + p1.getAge());
+        System.out.println("BloodType: " + p1.getBloodData().getBloodtype() + ((p1.getBloodData().getRHFactor()) ? "+" : "-"));
+
+        Patient p2 = new Patient(12 , 24 , new BloodData("A" , false));
+        System.out.println("ID: " + p2.getIdNumbrer() + "\nAge: " + p2.getAge());
+        System.out.println("BloodType: " + p2.getBloodData().getBloodtype() + ((p2.getBloodData().getRHFactor()) ? "+" : "-"));
+
     }
 }
