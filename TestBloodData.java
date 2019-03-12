f9f51c0fc1859a54de6b33625ea840693d6f6401
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 22:05:53 +0330

    Adds main method to test constructor of BloodData

--- TestBloodData.java
+++ TestBloodData.java
@@ -1,4 +1,11 @@
 package HomeWork4;
 
 public class TestBloodData {
+    public static void main(String[] args) {
+        BloodData Blood1= new BloodData();
+        System.out.println(Blood1.Bloodtype + ((Blood1.RHFactor)?  "+" : "-") );
+        BloodData Blood2= new BloodData("AB" ,true);
+        System.out.println(Blood2.Bloodtype + ((Blood2.RHFactor)?  "+" : "-") );
+
+    }
 }
