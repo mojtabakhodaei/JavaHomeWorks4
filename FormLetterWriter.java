832a5095d8416e5fa2bd7a778eb2c026dc2d76e4
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 17:07:32 +0330

    Add displaySalutation method number 2

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -1,8 +1,11 @@
 package HomeWork4;
 
 public class FormLetterWriter {
-    void displaySalutation(String lastName){
+    void displaySalutation(String lastName) {
         System.out.println("salutation Dear mr. or Ms. " + lastName);
     }
 
+    void displaySalutation(String firstName, String lastName) {
+        System.out.println("greeting Dear " + firstName + ' ' + lastName);
+    }
 }
