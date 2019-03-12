bcfbfd132e71d71e4fa44e8f24dfdae35c92a22f
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 19:05:09 +0330

    Adds main method for testing the methods

--- Billing.java
+++ Billing.java
@@ -12,4 +12,11 @@ public class Billing {
     static double computeBill(int price, int quantity , int coupon) {
         return (price * quantity - coupon)* 1.08;
     }
+
+    public static void main(String[] args) {
+        System.out.println(computeBill(1000));
+        System.out.println(computeBill(2000 , 4));
+        System.out.println(computeBill(3000 , 7 , 2500));
+              
+    }
 }
