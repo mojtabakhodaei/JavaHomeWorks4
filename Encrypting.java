12a164fd8693ddd0412087e15d4b4f8c20c30bb7
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 15:37:32 +0330

    import scanner declare string and integer get string and integer from user

--- Encrypting.java
+++ Encrypting.java
@@ -1,6 +1,17 @@
 package HomeWork4;
 
+import java.util.Scanner;
+
 public class Encrypting {
+
     public static void main(String[] args) {
+        String str;
+        int number;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write your sentences : ");
+        str = scanner.nextLine();
+        System.out.println("Write your number for encrypt: ");
+        number = scanner.nextInt();
     }
 }
