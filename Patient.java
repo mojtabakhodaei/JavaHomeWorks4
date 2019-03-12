c112fb713ff182ac170e5c902c5610eb1d11768e
mojtaba <mojtaba_cs@yahoo.com>
2019-03-12 22:10:11 +0330

    Create patient class includes an ID number, age, and BloodData. Adds default constructor

---
+++ Patient.java
@@ -0,0 +1,13 @@
+package HomeWork4;
+
+public class Patient {
+    int idNumbrer;
+    int age;
+    BloodData bloodData;
+
+    public Patient() {
+        idNumbrer = 0;
+        age = 0;
+        bloodData = new BloodData();
+    }
+}
